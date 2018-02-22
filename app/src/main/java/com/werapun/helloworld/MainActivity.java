a69cf0932df29e871e143fb5dd773b10ce9fd3a3
popzka <poppular19@hotmail.com>
2018-02-22 23:32:46 +0700

    Lab 4

---
+++ app/src/main/java/com/werapun/helloworld/MainActivity.java
@@ -0,0 +1,71 @@
+package com.werapun.helloworld;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tvHello;
+    private ImageView ivLogo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.werapun.helloworld.R.layout.activity_main);
+
+        tvHello = findViewById(com.werapun.helloworld.R.id.tvHello);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+                tvHello.setText("Name: Sasatorn Piromrod\n" +
+                        "Student ID: 5735512052 \n"+
+                        "Age: 22\n" +
+                        "E-mail: poppular19@hotmail.com\n" +
+                        "Phone: 080-9544259\n" +
+                        "Hobbit: Sing a song, Swimming");
+
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(com.werapun.helloworld.R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == com.werapun.helloworld.R.id.action_settings) {
+            ivLogo.setVisibility( View.VISIBLE );
+            System.out.println( ivLogo.isEnabled() );
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
